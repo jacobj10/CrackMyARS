228c52223fb55150cae2d8239bb6e15de7f4705a
jacobj10 <jacobj10@bbhcsd.org>
2017-04-25 23:23:02 -0400

    added tests

---
+++ attacks/crt.py
@@ -0,0 +1,45 @@
+import gmpy
+
+def find_invpow(x,n):
+    """Finds the integer component of the n'th root of x,
+    an integer such that y ** n <= x < (y + 1) ** n.
+    """
+    high = 1
+    while high ** n <= x:
+        high *= 2
+    low = high/2
+    while low < high:
+        mid = (low + high) // 2
+        if low < mid and mid**n < x:
+            low = mid
+        elif high > mid and mid**n > x:
+            high = mid
+        else:
+            return mid
+    return mid + 1
+
+def prod_minus_index(arr, i):
+    """
+    Product of a list excluding the index i
+    """
+    total = 1
+    counter = 0
+    while counter < len(arr):
+        if counter != i:
+            total *= arr[counter]
+        counter += 1
+    return total
+
+def crt(e, c, n):
+    if not isinstance(c, list) or not isinstance(n, list):
+        return -2 
+    if len(c) != e or len(n) != e:
+        return -1
+    m = 0
+    for k in range(e):
+        prod = prod_minus_index(n, k)
+        m += c[k] * prod * gmpy.invert(prod, n[k])
+    i = gmpy.mpz(m % (prod_minus_index(n, len(n))))
+    hd = hex(i.root(e)[0])[2:]
+    asc = bytes.fromhex(hd).decode('utf-8')
+    return asc
